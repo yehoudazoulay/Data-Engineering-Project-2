e3589134adb9924a4d7f31e394d5368c04016139
yehoudazoulay <yehoudazoulay@gmail.com>
2020-12-14 15:06:20 +0100

    test updated

--- test_app.py
+++ test_app.py
@@ -9,11 +9,6 @@ class FlaskTests(unittest.TestCase):
 	def tearDown(self):
 		pass
 
-	#test if the site respond 
-	def test_a_interface(self):
-		response = requests.get('http://localhost:5000')
-		self.assertEqual(response.status_code,200)
-
 
 if __name__=='__main__':
 	unittest.main()
