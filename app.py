9775e1464fa02701a167f187b2ed263ce5d12d0b
yehoudazoulay <yehoudazoulay@gmail.com>
2020-12-14 16:34:11 +0100

    elastic ingest and query

--- app.py
+++ app.py
@@ -2,10 +2,49 @@ from flask import Flask, request, render_template, flash
 import json
 import pandas as pd
 import os
+from elasticsearch import Elasticsearch, helpers
+import time
 
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
 
+#return the best match
+def submit_txt(es, index, txt):
+	query=es.search(index=index, 
+		 body={
+	 "query": {
+	     "match": {
+		 "text.english": {
+		     "query": txt,
+		     }
+		 }
+	     },
+	 "size": 20
+	    }
+	)
+	#send the best similar tweets to the web api
+	for el in query['hits']['hits']:
+            flash(el['_id'])
+            flash(el['_source']['text'])
+            flash(el['_score'])
+	return render_template('interface.html')
+
+def create_index(es, index, mapping):
+	print("Creating index...\n")
+	response = es.indices.create(
+	    index=index,
+	    body=mapping,
+	    ignore=400)
+	if 'acknowledged' in response:
+		if response['acknowledged'] == True:
+			print ("INDEX MAPPING SUCCESS FOR INDEX:", response['index'])
+	return index
+
+def ingest_data(es, index, data):
+	print("Ingesting data to ES...\n")
+	all_tweets=({'_id': doc['id'],'text': doc['text'], '_index': index } for doc in data)
+	helpers.bulk(es,all_tweets)
+	print("Data loaded into ES.\n")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -18,6 +57,14 @@ def index():
 
 
 if __name__ == '__main__':
+	print("Lauching the wep interface...\n")
+	print("Connecting to Elastic...\n")
+	es = Elasticsearch(['http://elasticsearch:9200/'], verify_certs=True)
+	while not es.ping():
+		time.sleep(5)
+	
+	print("Connected to Elastic\n")
+	print("Retrieving data from CSV...\n")
 	#retrieving the csv keep only 2columns
 	df= pd.read_csv('tweets.csv')
 	df=df[['id','text']]
@@ -29,6 +76,14 @@ if __name__ == '__main__':
 	    data=file.read()
 	data=json.loads(data)
 	
+	#load the mapping
+	with open('mapping.json') as file:
+	    mapping=file.read()
+	mapping=json.loads(mapping)
+	
+	index = create_index(es, 'tweets_trump', mapping)
+	ingest_data(es, index, data)
+	time.sleep(5)
 	app.run(host='0.0.0.0')
 	
 	
